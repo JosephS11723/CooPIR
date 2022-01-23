fb1c8bac1f9a39f961d113e6f28b901e3701f9f0
Jonah Fitzgerald <jonahfitzgerald0113@gmail.com>
2022-01-22 23:00:55 -0600

    added new fubctions that creates an admin role and an api role; api has read / write access on Users and Cases

--- mongo/setup.js
+++ mongo/setup.js
@@ -1,10 +1,42 @@
 conn = new Mongo();
 
+
+//---------------------------------------------
+
+db = conn.getDB("admin");
+
+db.createUser(
+    {
+      user: "myUserAdmin",
+      pwd: "securepassword",
+      roles: [
+        { role: "userAdminAnyDatabase", db: "admin" },
+        { role: "readWriteAnyDatabase", db: "admin" }
+      ]
+    }
+  )
+
+  db.createUser(
+    {
+        user: "api",
+        pwd: "bingusmalingus",
+        roles: [
+          { role: "readWrite", db: "Cases" },
+          { role: "readWrite", db: "Users" }
+        ]
+      }
+  )
+
+//---------------------------------------------
 db = conn.getDB("Users");
 
 db.createCollection("User");
 db.createCollection("Authentication");
+db.createCollection("Aggregations");
 
 db = conn.getDB("Cases");
 
 db.createCollection("Case");
+db.createCollection("Aggregations");
+
+//-----------------------------------------------
