569112ec496025972adbebbef55a0aaaf1187a35
Jonah Fitzgerald <jonahfitzgerald0113@gmail.com>
2022-02-07 14:50:39 -0600

    created a python script that generates sample data--ot currently only generates the data for Case and User collections

---
+++ scripts/generateSampleData.py
@@ -0,0 +1,63 @@
+from pymongo import MongoClient
+from random import randint
+
+Users = [
+
+    {
+        "name":"Alex Petty",
+        "email":"alexpetty@yahoo.org",
+        "role":"responder",
+        "cases":[],
+        "saltedHash":"binka"
+    },
+    {
+        "name":"Andrew Merrow",
+        "email":"andrewmerrow@yahoo.org",
+        "role":"responder",
+        "cases":[],
+        "saltedHash":"binka"
+    },
+    {
+        "name":"Sarah Siharath",
+        "email":"sarahsiharath@yahoo.org",
+        "role":"responder",
+        "cases":[],
+        "saltedHash":"binka"
+    },
+    {
+        "name":"Heath Tims",
+        "email":"timbo@yahoo.org",
+        "role":"supervisor",
+        "cases":[],
+        "saltedHash":"binka"
+    }
+
+]
+
+################################################
+
+conn = MongoClient("mongodb://api:bingusmalingus@localhost:27017")
+
+
+coll = conn["Users"]["User"]
+
+coll.insert_many(Users)
+
+
+coll = conn["Cases"]["Case"]
+
+for i in range(0,20):
+    coll.insert_one(
+        {
+            "name": "case"+str(i),
+
+            "dateCreated": "Decembruary " + str(randint(1,31)) + ", 1969",
+
+            "viewAccess": "responder",
+
+            "editAccess": "supervisor",
+
+            "collabs": ["Heath Tims", "Andrew Merrow"]
+        }
+    )
+
