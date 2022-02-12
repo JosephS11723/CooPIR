6476089aeff915c6886e0c6c5be53acf44c32d9c
bv <43678349+brandonvessel@users.noreply.github.com>
2022-02-11 20:23:12 -0600

    Removed ftp, changed tokenAuth todo

--- src/api/lib/security/tokenAuth.go
+++ src/api/lib/security/tokenAuth.go
@@ -3,6 +3,6 @@ package security
 import "github.com/gin-gonic/gin"
 
 func VerifyToken(c *gin.Context) bool {
-	// TODO this is where the token verification code lives
+	// TODO: this is where the token verification code lives
 	return true
 }
