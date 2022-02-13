5b0fa275eb7f18f759e1700cb0b0d34e30dc96fc
Jonah Fitzgerald <jonahfitzgerald0113@gmail.com>
2022-02-12 20:28:36 -0600

    started implementing the JobQueue struct

--- src/api/lib/jobqueue/jobQueue.go
+++ src/api/lib/jobqueue/jobQueue.go
@@ -1,17 +1,33 @@
 package jobqueue
 
+import (
+	"github.com/JosephS11723/CooPIR/src/api/lib/jobservertypes"
+)
+
 type JobQueue struct {
-	NewJobs []NewJob
+	NewJobs []jobservertypes.NewJob
+	Workers []WorkerInfo
 }
 
-func (jq JobQueue) push(job_request NewJob) {
+type WorkerInfo struct {
+	Name           string
+	Host           string
+	Responsibility string
+}
+
+func (jq JobQueue) push(job_request jobservertypes.NewJob) {
 
 	jq.NewJobs = append(jq.NewJobs, job_request)
 
 }
 
-func (jq JobQueue) pop() {
+func (jq JobQueue) pop() jobservertypes.NewJob {
 
+	var item jobservertypes.NewJob
+
+	item, jq.NewJobs = jq.NewJobs[0], jq.NewJobs[1:]
+
+	return item
 }
 
 func ManageQueue(receiver <-chan interface{}) {
