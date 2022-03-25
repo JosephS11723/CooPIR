9460fb6b661866b64e38fd89549329b0bc2a7d67
Jonah Fitzgerald <jonahfitzgerald0113@gmail.com>
2022-03-25 18:57:28 -0500

    added unmarshalling to DbCreateCase

--- src/api/handlers/iodb/db.go
+++ src/api/handlers/iodb/db.go
@@ -5,6 +5,8 @@ import (
 	//"strconv"
 
 	//"github.com/JosephS11723/CooPIR/src/api/lib/dbInterface"
+	"github.com/JosephS11723/CooPIR/src/api/lib/dbInterface"
+	"github.com/JosephS11723/CooPIR/src/api/lib/dbtypes"
 	"github.com/gin-gonic/gin"
 	//"go.mongodb.org/mongo-driver/bson"
 	//"go.mongodb.org/mongo-driver/mongo"
@@ -15,6 +17,12 @@ func DbGetCaseInfo(c *gin.Context) {
 }
 
 func DbCreateCase(c *gin.Context) {
+
+	var json_request dbtypes.Case
+
+	c.BindJSON(&json_request)
+
+	dbInterface.MakeCase(json_request)
 
 }
 
