a741d35a9c3ec36ec3e70085e3ce3a59daef8bea
Jonah Fitzgerald <jonahfitzgerald0113@gmail.com>
2022-03-26 22:23:42 -0500

    added an UpdateCase struct

--- src/api/handlers/iodb/db.go
+++ src/api/handlers/iodb/db.go
@@ -1,13 +1,14 @@
 package iodb
 
 import (
-	//"log"
-	//"strconv"
 
 	//"github.com/JosephS11723/CooPIR/src/api/lib/dbInterface"
+	"log"
+
 	"github.com/JosephS11723/CooPIR/src/api/lib/dbInterface"
 	"github.com/JosephS11723/CooPIR/src/api/lib/dbtypes"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	//"go.mongodb.org/mongo-driver/bson"
 	//"go.mongodb.org/mongo-driver/mongo"
 )
@@ -28,11 +29,31 @@ func DbCreateCase(c *gin.Context) {
 
 func DbUpdateCase(c *gin.Context) {
 
+	var json_request interface{}
+
 	//this is NOT the correct code for
-	var json_request dbtypes.Case
+	byte_data, err := c.GetRawData()
 
-	c.BindJSON(&json_request)
+	if err != nil {
 
-	dbInterface.MakeCase(json_request)
+		log.Panicln(err)
+
+	}
+
+	bson.UnmarshalExtJSON(byte_data, true, json_request)
+
+	filter := bson.M{"email": "test0@test.com"}
+
+	/*
+		update := bson.D{{"$set",
+			bson.D{
+				{"name", name},
+			},
+		}}
+	*/
+
+	dbInterface.UpdateDoc("Cases", "CaseMetadata", filter, update)
+
+	//dbInterface.MakeCase(json_request)
 
 }
---
+++ src/api/lib/dbtypes/dbUpdateTypes.go
@@ -0,0 +1,6 @@
+package dbtypes
+
+type UpdateCase struct {
+	Filter map[string]interface{}
+	Case   map[string]interface{}
+}
