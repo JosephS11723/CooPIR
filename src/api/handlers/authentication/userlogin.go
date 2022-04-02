bb5d6d791c884613ed405c6400defae4a883c397
brandonvessel <43678349+brandonvessel@users.noreply.github.com>
2022-04-02 15:04:30 -0500

    re-added removed code

--- src/api/handlers/authentication/userlogin.go
+++ src/api/handlers/authentication/userlogin.go
@@ -8,7 +8,11 @@ import (
 // Basic user login fucntion
 func userLogin(email string, password string) bool {
 	// get password hash from database
-	hash := dbInterface.RetrieveHashByEmail(email)
+	hash, err := dbInterface.RetrieveHashByEmail(email)
+
+	if err != nil {
+		return false
+	}
 
 	// Check the password against the hash
 	result := security.CheckPass(password, hash)
