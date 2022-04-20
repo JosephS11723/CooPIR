a10b57d74e068be81d7fd47763fe6eece2d924a3
Jonah Fitzgerald <jonahfitzgerald0113@gmail.com>
2022-04-19 20:06:20 -0500

    implemented the Worker type

--- src/api/handlers/iojobs/workers.go
+++ src/api/handlers/iojobs/workers.go
@@ -1,17 +1,20 @@
 package iojobs
 
+import "github.com/gin-gonic/gin"
+
+//used for creating a new job worker
+func CreateWorker(c *gin.Context) {
+
+}
+
+func CancelWorker() {
+
+}
+
 func GetWorkerInfo() {
 
 }
 
 func ReceiveResult() {
-
-}
-
-func CreateWorker() {
-
-}
-
-func CancelWorker() {
 
 }
--- src/api/lib/dbtypes/dbJob.go
+++ src/api/lib/dbtypes/dbJob.go
@@ -1,31 +1,31 @@
 package dbtypes
 
 type Job struct {
-	JobUUID       string   `json:"jobuuid"`
-	Arguments     []string `json:"arguments"`
-	Name          string   `json:"name"`
-	JobType       string   `json:"jobtype"`
-	Status        Status   `json:"status"`
-	StartTime     int      `json:"starttime"`
-	EndTime       int      `json:"endtime"`
-	JobResultUUID string   `json:"jobresultuuid"`
+	JobUUID       string    `json:"jobuuid"`
+	Arguments     []string  `json:"arguments"`
+	Name          string    `json:"name"`
+	JobType       string    `json:"jobtype"`
+	Status        JobStatus `json:"status"`
+	StartTime     int       `json:"starttime"`
+	EndTime       int       `json:"endtime"`
+	JobResultUUID string    `json:"jobresultuuid"`
 }
 
 //this is the status
 //of
 //the job struct
-type Status string
+type JobStatus string
 
 //possible values for Status
 const (
-	Queued     Status = "queued"
-	InProgress Status = "in-progress"
-	Finished   Status = "finished"
-	JobError   Status = "error"
-	Cancelled  Status = "cancelled"
+	Queued     JobStatus = "queued"
+	InProgress JobStatus = "in-progress"
+	Finished   JobStatus = "finished"
+	JobError   JobStatus = "error"
+	Cancelled  JobStatus = "cancelled"
 )
 
-func (s Status) String() string {
+func (s JobStatus) String() string {
 	var return_val string
 
 	switch s {
