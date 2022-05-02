e7cf14f556059b02d143738d4ce57faa391947b8
brandonvessel <43678349+brandonvessel@users.noreply.github.com>
2022-05-02 17:24:31 -0500

    Adding worker authentication and logs

--- src/api/handlers/iojobs/workers.go
+++ src/api/handlers/iojobs/workers.go
@@ -3,9 +3,12 @@ package iojobs
 import (
 	"net/http"
 
+	"github.com/JosephS11723/CooPIR/src/api/config"
 	"github.com/JosephS11723/CooPIR/src/api/lib/coopirutil"
+	"github.com/JosephS11723/CooPIR/src/api/lib/crypto"
 	"github.com/JosephS11723/CooPIR/src/api/lib/dbInterface"
 	"github.com/JosephS11723/CooPIR/src/api/lib/dbtypes"
+	"github.com/JosephS11723/CooPIR/src/api/lib/logtypes"
 	"github.com/gin-gonic/gin"
 )
 
@@ -32,7 +35,6 @@ func CreateWorker(c *gin.Context) {
 	}
 
 	uuid, err := dbInterface.MakeWorker(NewWorker)
-
 	if err != nil {
 		c.AbortWithStatusJSON(
 			http.StatusInternalServerError,
@@ -42,8 +44,21 @@ func CreateWorker(c *gin.Context) {
 		)
 	}
 
-	c.JSON(200, gin.H{"uuid": uuid})
+	// create token for worker
+	token, err := crypto.CreateToken(uuid)
 
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not create token"})
+		return
+	}
+
+	// worker creation log
+	dbInterface.MakeCaseLog(c, logtypes.LogDefaultCaseUUID, uuid, dbtypes.Info, logtypes.WorkerStart, nil)
+
+	// set token in cookie
+	c.SetCookie("token", token, 9999999, "", "", false, config.HTTPOnly)
+
+	c.JSON(200, gin.H{"uuid": uuid})
 }
 
 func CancelWorker() {
